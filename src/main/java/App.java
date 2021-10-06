86cc35f71880f983177bf524106a3cee15bec4cb
aytankhalilova <aytan01khalilova@gmail.com>
2021-10-06 15:06:09 -0400

    lab1

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println("This is a simple App.\nIt is created for testing by Aytan Khalilova.");
+    }
+}
