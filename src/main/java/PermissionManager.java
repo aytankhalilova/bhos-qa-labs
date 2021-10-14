1943a0cc67d76bd097c727ead5f80906bc83d011
aytankhalilova <64959696+aytankhalilova@users.noreply.github.com>
2021-10-14 11:07:33 +0400

    Update PermissionManager.java

--- src/main/java/PermissionManager.java
+++ src/main/java/PermissionManager.java
@@ -2,15 +2,25 @@ public class PermissionManager {
     private PermissionLevel mCurrentLevel = PermissionLevel.USER;
 
     public String getPermissionLevel() {
-        String role = switch (mCurrentLevel) {
-            case DEVELOPER -> "DEVELOPER";
-            case ADMIN -> "ADMIN";
-            case USER -> "USER";
-            default -> throw new IllegalStateException(String.valueOf(mCurrentLevel));
-        };
+        String role;
 
-        String result = role;
-        return result;
+        switch(mCurrentLevel) {
+
+            case DEVELOPER:
+                role = "DEVELOPER";
+                break;
+
+            case ADMIN:
+                role = "ADMIN";
+                break;
+
+            case USER:
+                role = "USER";
+                break;
+            default:
+                throw new IllegalStateException("RESULT: " + mCurrentLevel);
+        }
+        return role;
     }
 
     public void get_and_setPermissionLevel(PermissionLevel permissionLevel) {
